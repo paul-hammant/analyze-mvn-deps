2d9ab94ba70bf4fb32be76e3ffe2f0b324f9783a
Paul Hammant <paul@hammant.org>
2018-02-03 10:03:19 -0500

    grep-o silently crashing for some reason

--- analyze-deps.sh
+++ analyze-deps.sh
@@ -33,8 +33,8 @@ do
       | sed -E -e 's/[[:blank:]]+/ /g' | sed 's/\<//g' | sed 's/\"/ /g' | sed 's/^a //' | sed 's/\>//g' \
       | grep href | sed '/maven-metadata/d' | sed 's/.*title= //' | sed '/\.\./d' | grep '\:' \
       | sed -E 's/(.*) (.*) (.*) (.*) (.*) (.*) (.*)/\5 \6 \1/' \
-      | sed 's/href //g' | sed 's/\///' | sed '/-[aA]lpha[0-9]* /d' | sed '/-[bB]eta[0-9]* /d' \
-      | sed '/-[Rr][Cc][0-9]* /d' | sed '/-SNAPSHOT /d' \
+      | sed 's/href //g' | sed 's/\///' | sed '/-[aA]lpha[\.]*[0-9]* /d' | sed '/-[bB]eta[\.]*[0-9]* /d' \
+      | sed '/-[Rr][Cc][\.]*[0-9]* /d' | sed '/-SNAPSHOT /d' \
       | sed 's/  title$//' | sort -nr | uniq > .deps/tmp_list_of_versions.txt
 
     cat .deps/tmp_list_of_versions.txt | sed "s/ $version$/ $version \*\*\*/" > .deps/tmp_list_of_versions_with_current_indicated.txt
@@ -45,7 +45,7 @@ do
 
     matches=$(cat .deps/dependencies-tree.txt | grep "$group:$artifact")
     matchesCount=$(echo "$matches" | wc -l | tr -d '[:space:]')
-    leastTransitiveDep=$(echo "$matches" | sort | tail -1 | grep -o ' ' | grep -c .)
+    leastTransitiveDep=$(echo "$matches" | sort | tail -1 | tr -cd ' ' | wc -m)
     echo " - a level $leastTransitiveDep dependency among $matchesCount (possibly transitive) uses\n" >> .deps/big-dependency-report.txt
 
     cat  .deps/tmp_list_of_versions_since_current.txt >> .deps/big-dependency-report.txt
